6a5125c688dfd6e046cbb101b733e089951ec11d
Markus Helm <markus@hanshelm.de>
2024-11-14 11:13:09 +0100

    midi clock mutli processing + bat scripts

--- midi_handler.py
+++ midi_handler.py
@@ -1,35 +1,36 @@
 import mido
 from color_extractor import ColorExtractor
-import threading
+import multiprocessing
 import time
 
 class MidiHandler:
     def __init__(self):
-        try:
-            self.port = mido.open_output(name="AutoLight 1")
-        except OSError:
-            print("MIDI port \"AutoLight\" not found!")
-            self.port = None
-
         self.SECONDARY_OFFSET = 20
         self.bpm = 128
-        self.running = False
-        self.clock_thread = None
+        #self.running = False
+        self.clock_process = None
         
-    def send_primary_color(self, hue):
-        if self.port is not None:
-            self.port.send(mido.Message("note_on", note=hue, velocity=127))
-    def send_secondary_color(self, hue):
-        if self.port is not None:
-            self.port.send(mido.Message("note_on", note=hue+self.SECONDARY_OFFSET, velocity=127))
+    #def send_primary_color(self, hue):
+    #    if self.port is not None:
+    #        self.port.send(mido.Message("note_on", note=hue, velocity=127))
+    #def send_secondary_color(self, hue):
+    #    if self.port is not None:
+    #        self.port.send(mido.Message("note_on", note=hue+self.SECONDARY_OFFSET, velocity=127))
 
-    def send_clock_tick(self):
-        if self.port is not None:
-            self.port.send(mido.Message("clock"))
+    def getMidiPort(self, port_name):
+        for port in mido.get_output_names():
+            if port_name in port:
+                try:
+                    return mido.open_output(name=port)
+                except OSError:
+                    print(f"Could not open MIDI port \"{port}\"!")
+                    return None
+        print(f"MIDI port \"{port_name}\" not found!")
 
     def set_bpm(self, bpm):
         self.bpm = bpm if bpm > 0 else 128 # Update the BPM value
-        if self.running:  # Restart the clock to synchronize
+        #if self.running:  # Restart the clock to synchronize
+        if self.clock_process and self.clock_process.is_alive():
             print(f"Set BPM: {self.bpm}")
             self.stop_clock()
             self.start_clock()
@@ -37,16 +38,18 @@ class MidiHandler:
     def get_bpm(self):
         return self.bpm
 
-    def send_clock_signals(self):
-        while self.running:
-            if self.bpm < 3:
-                self.bpm = 128
-            quarter_note_duration = 60 / (self.bpm+2)
-            tick_duration = quarter_note_duration / 24
+    def send_clock_signals(self, bpm, port_name):
+        port = self.getMidiPort(port_name)
+        #while self.running:
+        #    if self.bpm < 3:
+        #        self.bpm = 128
+        quarter_note_duration = 60 / (bpm + 2)
+        tick_duration = quarter_note_duration / 24
+        while True:
             start_time = time.perf_counter()
             # Send 24 clock ticks per beat (each clock tick is 1/24 of a beat)
             for _ in range(24):
-                self.send_clock_tick()
+                port.send(mido.Message("clock"))
                 elapsed_time = time.perf_counter() - start_time
                 time_to_sleep = tick_duration - elapsed_time
                 if time_to_sleep > 0:
@@ -54,31 +57,31 @@ class MidiHandler:
                 start_time = time.perf_counter()  # Reset start time for the next tick
 
     def start_clock(self):
-        if not self.running:
-            self.running = True
-            self.clock_thread = threading.Thread(target=self.send_clock_signals)
-            self.clock_thread.start()
+        if not self.clock_process or not self.clock_process.is_alive():
+            self.clock_process = multiprocessing.Process(
+                target=self.send_clock_signals, args=(self.bpm, "AutoLight"))
+            self.clock_process.start()
 
     def stop_clock(self):
-        if self.running:
-            self.running = False
-            self.clock_thread.join()
+        if self.clock_process and self.clock_process.is_alive():
+            self.clock_process.terminate()
+            self.clock_process.join()
 
-def main():
-    midi = MidiHandler()
-    colorex = ColorExtractor()
-    print("Primary Colors...")
-    for idx, midiHue in enumerate(colorex.AVAILABLE_HUES):
-        print(midiHue)
-        input()
-        midi.send_primary_color(idx)
-    print("Secondary Colors...")
-    for idx, midiHue in enumerate(colorex.AVAILABLE_HUES):
-        print(midiHue)
-        input()
-        midi.send_secondary_color(idx)
-    input()
+#def main():
+#    midi = MidiHandler()
+#    colorex = ColorExtractor()
+#    print("Primary Colors...")
+#    for idx, midiHue in enumerate(colorex.AVAILABLE_HUES):
+#        print(midiHue)
+#        input()
+#        midi.send_primary_color(idx)
+#    print("Secondary Colors...")
+#    for idx, midiHue in enumerate(colorex.AVAILABLE_HUES):
+#        print(midiHue)
+#        input()
+#        midi.send_secondary_color(idx)
+#    input()
 
 if __name__ == "__main__":
-    main()
+    print(mido.get_output_names())
 
---
+++ run.bat
@@ -0,0 +1,3 @@
+call .venv/Scripts/activate.bat
+pythonw main.py
+pause
